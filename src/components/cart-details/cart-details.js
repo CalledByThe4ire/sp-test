e220aacdd06a733a26f7d03d1f4eea99c6f3f166
Andrey Karamnoff <karamnoff@gmail.com>
2021-08-22 12:34:34 +0300

    Add input, label, button, order-discount; complete styles for order

--- src/components/cart-details/cart-details.js
+++ src/components/cart-details/cart-details.js
@@ -2,7 +2,12 @@ import store from '../../assets/js/store';
 import { removeItem, updateItemQuantity } from '../../assets/js/slices/order';
 
 document.addEventListener(`DOMContentLoaded`, () => {
-	const cartDetailsContainer = document.querySelector('.cart-details__list');
+	const cartDetails = document.querySelector('.cart-details');
+	let cartDetailsContainer = null;
+
+	if (cartDetails) {
+		cartDetailsContainer = cartDetails.querySelector('.cart-details__list');
+	}
 
 	const handleChangeClick = (evt, payload) => {
 		const {
@@ -34,13 +39,15 @@ document.addEventListener(`DOMContentLoaded`, () => {
 					 src="assets/images/item-${imageName}.jpg"
 					 srcset="assets/images/item-${imageName}@2x.jpg 2x"
 					 alt="${imageName}" />
+
 				<div class="item__inner">
 					<p class="item__name">${name}</p>
 					<div class="item__quantity">
-						<button class="item__btn item__btn--dec" data-id=${id}">-</button>
-						<span>${quantity}</span>
-						<button class="item__btn item__btn--inc" data-id=${id}">+</button>
-					<span class="item__price">${price}</span>
+						<button class="item__btn item__btn--dec" data-id=${id}"><span>&#8211;</span></button>
+						<div class="item__count">${quantity}</div>
+						<button class="item__btn item__btn--inc" data-id=${id}"><span>+</span></button>
+						<div class="item__price">$&nbsp;${price}</div>
+					</div>
 				</div>
 				<button class="item__btn item__btn--remove item-btn" data-id=${id}">
 					<svg class="item-btn__icon icon icon--cross" style="width: 18px; height: 18px;">
@@ -52,6 +59,10 @@ document.addEventListener(`DOMContentLoaded`, () => {
 		`;
 
 	const render = () => {
+		cartDetails.classList.toggle(
+			'cart-details--invisible',
+			store.getState().order.items.length === 0
+		);
 		if (cartDetailsContainer) {
 			cartDetailsContainer.innerHTML = store
 				.getState()
