90f80f79d565a73db3338bc06f00f14b4731c389
Andrey Karamnoff <karamnoff@gmail.com>
2021-08-23 20:10:54 +0300

    Update js for payment-info, stages

--- src/components/payment-info/payment-info.js
+++ src/components/payment-info/payment-info.js
@@ -4,23 +4,27 @@ import store from '../../assets/js/store';
 document.addEventListener(`DOMContentLoaded`, () => {
 	const { activeIndex } = store.getState().paymentProcess;
 
-	document.querySelectorAll('.payment-info').forEach((form, index) => {
-		if (index === activeIndex) {
-			form.classList.remove('payment-info--invisible');
+	const forms = document.querySelectorAll('.payment-info');
 
-			form.querySelectorAll('[name*="credit-card-number"]').forEach(input => {
-				IMask(input, {
-					mask: '0000',
+	if (forms) {
+		forms.forEach((form, index) => {
+			if (index === activeIndex) {
+				form.classList.remove('payment-info--invisible');
+
+				form.querySelectorAll('[name*="credit-card-number"]').forEach(input => {
+					IMask(input, {
+						mask: '0000',
+					});
 				});
-			});
 
-			IMask(form.querySelector('[name="credit-card-expire-date"]'), {
-				mask: '00{/}00',
-			});
+				IMask(form.querySelector('[name="credit-card-expire-date"]'), {
+					mask: '00{/}00',
+				});
 
-			IMask(form.querySelector('[name="credit-card-cvv"]'), {
-				mask: '000',
-			});
-		}
-	});
+				IMask(form.querySelector('[name="credit-card-cvv"]'), {
+					mask: '000',
+				});
+			}
+		});
+	}
 });
--- src/components/stages/stages.js
+++ src/components/stages/stages.js
@@ -3,11 +3,13 @@ import store from '../../assets/js/store';
 document.addEventListener(`DOMContentLoaded`, () => {
 	const { activeIndex } = store.getState().paymentProcess;
 
-	Array.from(document.querySelectorAll('.stages__stage'))
-		.reverse()
-		.forEach((stage, index) => {
+	const stages = Array.from(document.querySelectorAll('.stages__stage'));
+
+	if (stages.length !== 0) {
+		stages.reverse().forEach((stage, index) => {
 			if (index === activeIndex) {
 				stage.classList.add('stage--active');
 			}
 		});
+	}
 });
